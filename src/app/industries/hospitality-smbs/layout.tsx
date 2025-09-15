2fe97293140e50881139df53b4f6dbb373173896
quadcydle-dev <quadcydletest29@gmail.com>
2025-09-15 20:46:59 +0530

    Update layout.tsx

--- src/app/industries/hospitality-smbs/layout.tsx
+++ src/app/industries/hospitality-smbs/layout.tsx
@@ -4,7 +4,7 @@ export const metadata: Metadata = {
     title: "Cdata",
     description:
         "Cdata",
-    alternates: { canonical: "https://cdatainsights.com/services/ai-ml" },
+    alternates: { canonical: "https://cdatainsights.com/industries/hospitality-smbs" },
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
