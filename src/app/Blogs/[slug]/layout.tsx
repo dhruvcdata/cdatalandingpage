0f6816396c3e3999f1149beda16b723f1a620416
dhruvagrawat <dhruvagrawat9@gmail.com>
2026-01-16 20:10:58 +0530

    yes

---
+++ src/app/Blogs/[slug]/layout.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Cdata",
+    description:
+        "Cdata",
+    alternates: { canonical: "https://cdatainsights.com/Blogs" },
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return <>{children}</>;
+}
